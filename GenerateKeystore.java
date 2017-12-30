3dfc0d1a8871bf2ca92d18b8f70079430b2f4c94
Mikael Gueck <gumi@iki.fi>
2017-12-30 13:17:18 +0200

    dox

---
+++ GenerateKeystore.java
@@ -0,0 +1,44 @@
+/*
+  KeyStore check by @mikaelhg
+  javac --release 8 GenerateKeystore.java
+  
+ */
+
+import java.io.ByteArrayOutputStream;
+import java.security.KeyStore;
+import java.util.*;
+
+class GenerateKeystore {
+
+    private static void storeAndPrint(final KeyStore keyStore, final String password) throws Exception {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            if (null == password) {
+                keyStore.store(baos, null);
+            } else {
+                keyStore.store(baos, password.toCharArray());
+            }
+            final byte firstByte = baos.toByteArray()[0];
+            System.out.printf("%15s - %02x\n", password, firstByte);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void testKeyStore(final KeyStore keyStore) throws Exception {
+        keyStore.load(null, null);
+        storeAndPrint(keyStore, "randompassword");
+        storeAndPrint(keyStore, "changeit");
+        storeAndPrint(keyStore, "");
+    }
+
+    public static void main(String[] args) throws Exception {
+        final String[] types = {"JKS", "PKCS12", KeyStore.getDefaultType()};
+        for (final String storeType : types) {
+            System.out.printf("\n%s\n", storeType);
+            final KeyStore keyStore = KeyStore.getInstance(storeType);
+            testKeyStore(keyStore);
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -6,8 +6,36 @@ java.security.InvalidAlgorithmParameterException: the trustAnchors parameter mus
 
 https://github.com/docker-library/openjdk/issues/145
 
+https://github.com/docker-library/openjdk/issues/19
+
+https://packages.debian.org/source/sid/ca-certificates-java
+
+http://hg.openjdk.java.net/jdk8u/jdk8u/jdk/file/67a15c76095d/src/share/classes/sun/security/provider/JavaKeyStore.java
+
 java -Djavax.net.ssl.trustStore=custompath/cacerts -Djavax.net.ssl.trustStorePassword=changeit
 
 -Djavax.net.debug=ssl:handshake
 
+```bash
+java -cp . TestHttps 'https://www.google.com/'
 
+java -Djavax.net.ssl.trustStorePassword=changeit -cp . TestHttps "https://www.google.com/"
+```
+
+-Djavax.net.ssl.trustStoreType=jks
+
+```bash
+
+apt update && apt -y install nano less file
+
+ls -Flah /etc/ssl/certs/java/cacerts
+file /etc/ssl/certs/java/cacerts
+
+rm /etc/ssl/certs/java/cacerts
+/var/lib/dpkg/info/ca-certificates-java.postinst configure
+
+
+docker run -it --rm openjdk:9-jdk find /usr/lib/jvm/java-9-openjdk-amd64 -type f -print0 | xargs -0 md5sum > sums.9-jdk
+docker run -it --rm openjdk:9-jdk-slim find /usr/lib/jvm/java-9-openjdk-amd64 -type f -print0 | xargs -0 md5sum > sums.9-jdk-slim
+
+```
